3ab50606af5b75574c38170f78709f07dc85a0b1
ljnchn <ljnchn@qq.com>
2026-02-28 09:55:17 +0800

    Remove Dashboard page and client script

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 export { InitPage } from './InitPage';
 export { LoginPage } from './LoginPage';
-export { DashboardPage } from './DashboardPage';
 export { HomePage } from './HomePage';
 export { ErrorPage } from './ErrorPage';
 export { Layout, PageLayout } from './Layout';
